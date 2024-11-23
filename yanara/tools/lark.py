4098c775f664f5aa95197a870ea67cffb06e53fa
lemorage <one.lemorage@gmail.com>
2024-11-23 23:03:25 +0800

    feat: add tool to get weekly report statistics

--- yanara/tools/lark.py
+++ yanara/tools/lark.py
@@ -38,7 +38,6 @@ def lookup_room_availability_by_date(self: "Agent", check_in: str, check_out: st
 
     lark_service = LarkTableService("KFo5bqi26a52u2s5toJcrV6tnWb")
 
-    # Fetch the records using the service
     return lark_service.fetch_records_within_date_range(
         table_id="tblxlwPlmWXLOHl7",
         view_id="vew9aCSfMp",
@@ -55,4 +54,79 @@ def lookup_room_availability_by_date(self: "Agent", check_in: str, check_out: st
         filter_field_name="日期",
         start_date=formatted_check_in,
         end_date=formatted_check_out,
+    )
+
+
+def get_weekly_report_statistics(self: "Agent", which_week: str) -> dict:
+    """Get the weekly report statistics for a specific week.
+
+    Args:
+        which_week (int): The week number to get the statistics for.
+
+    Returns:
+        dict: A dictionary containing the weekly report statistics.
+
+    Example:
+        >>> get_weekly_report_statistics(38)
+        {
+        'items': [
+            {
+                'fields': {
+                    '101已售房晚': {'type': 2, 'value': [6]},
+                    '201已售房晚': {'type': 2, 'value': [6]},
+                    '202已售房晚': {'type': 2, 'value': [7]},
+                    '301已售房晚': {'type': 2, 'value': [7]},
+                    '302已售房晚': {'type': 2, 'value': [6]},
+                    '401已售房晚': {'type': 2, 'value': [7]},
+                    'repar': {'type': 2, 'value': [12870.238095238095]},
+                    '周一日期': 1726412400000,
+                    '周日日期': {'type': 5, 'value': [1726930800000]},
+                    '売上': {'type': 2, 'value': [540550]},
+                    '平均房价': {'type': 2, 'value': [13860.25641025641]},
+                    '总儿童数': {'type': 2, 'value': [0]},
+                    '总泊数': {'type': 2, 'value': [39]},
+                    '有効注文数': {'type': 2, 'value': [16]},
+                    '稼働率': {'type': 2, 'value': [0.9285714285714286]},
+                    '第几周': {'type': 2, 'value': [38]},
+                    '総人数': {'type': 2, 'value': [40]},
+                    '総人泊数': {'type': 2, 'value': [100]}
+                },
+                'record_id': 'recukzcjtCfXkw'
+            }
+        ],
+        'has_more': False,
+        'total': 1
+    }
+
+
+    """
+    from yanara.api.lark_api.lark_service import LarkTableService
+
+    lark_service = LarkTableService("KFo5bqi26a52u2s5toJcrV6tnWb")
+
+    return lark_service.fetch_records_with_exact_value(
+        table_id="tblulMPBjoYKFpDg",
+        view_id="vew8UWgWyj",
+        field_names=[
+            "第几周",
+            "周一日期",
+            "周日日期",
+            "repar",
+            "総人数",
+            "総人泊数",
+            "总儿童数",
+            "总泊数",
+            "稼働率",
+            "売上",
+            "平均房价",
+            "有効注文数",
+            "101已售房晚",
+            "201已售房晚",
+            "202已售房晚",
+            "301已售房晚",
+            "302已售房晚",
+            "401已售房晚",
+        ],
+        filter_field_name="第几周",
+        filter_value=str(which_week),
     )
